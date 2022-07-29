089626c2e9e139e2bf7cce7e33bc40ac479937a4
Keith <keithwesley187@gmail.com>
2022-07-29 11:45:08 +0300

    Successfully fetching from heroku

--- src/Pages/FeedBackPage.js
+++ src/Pages/FeedBackPage.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import FeedBackComments from '../Components/FeedBackComments'
 import FeedBackForm from '../Components/FeedBackForm'
 
 function FeedBackPage(){
+
+  const [commentsZ, setCommentsZ] = useState([])
+
+    useEffect(() => {
+        fetch('https://phase2-api.herokuapp.com/userdata')
+        .then(r => r.json())
+        .then(data => setCommentsZ(data))
+    }, []);
+
+    function handlePosting(data){
+        setCommentsZ([...commentsZ, data])
+    }
+
+    function deleteComm(id){
+        fetch(`https://phase2-api.herokuapp.com/userdata/${id}`,{
+            method: "DELETE",
+        })
+        .then(r => r.json())
+        .then(() => {
+            const goThru = commentsZ.filter((comment) => comment.id !== id)
+                setCommentsZ(goThru)
+            })
+        }
+
   return (
     <div className='coinContainer'>
       <div className='sideBar'>
-        <FeedBackForm />
+        <FeedBackForm handlePosting={handlePosting} />
       </div>
-      <FeedBackComments />
+      <FeedBackComments commentEd={commentsZ} deleteComm={deleteComm}/>
     </div>
   )
 }
